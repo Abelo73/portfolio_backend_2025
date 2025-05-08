d1978732b3475b0220638573904e1a1afc36828a
Abelo73 <abeladisu73@gmail.com>
2025-05-08 08:49:17 +0300

    frontend url issue next issue fied

--- src/main/java/org/act/portfolio_backend_2025/config/WebConfig.java
+++ src/main/java/org/act/portfolio_backend_2025/config/WebConfig.java
@@ -14,8 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins(frontendUrl)
-                .allowedOrigins("*")
+                .allowedOrigins(frontendUrl)
+//                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
